7829550eca7695262263d3c588ec051b4daed328
sabjcoder <35679634+sabjcoder@users.noreply.github.com>
2018-01-23 18:44:35 +0530

    professional commit 2

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
     print(res)
