d46d3a872b2fd1346403c6f6b2970b55d8db73d5
sabjcoder <35679634+sabjcoder@users.noreply.github.com>
2018-01-29 18:41:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def makeWebhookResult(data):
         return {}
 
     # print(json.dumps(item, indent=4))
-    celsius = (units.get('temperature') - 32) / 1.8
+    celsius = 32
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + celsius
